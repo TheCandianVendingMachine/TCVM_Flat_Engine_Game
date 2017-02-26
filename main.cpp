cea3863293799e908ebe40288a106d33f6bab742
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-25 23:26:21 -0700

    Updated example for current engine iteration

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 // Flat Engine Game
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
-#include <fe/filesystem/fileUtilities.hpp>
+#include <fe/time/time.hpp>
+#include <fe/time/clock.hpp>
 #include <iostream>
+#include <thread>
 
 int main()
     {
-        auto first = fe::lastTimeModified("test.txt");
+        fe::clock test;
 
-        std::ofstream out("test.txt");
-        out << "a";
-        out.close();
-        
-        std::cout << "Difference: " << fe::lastTimeModified("test.txt") - first;
+        // Prints 0.500
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        test.stop(true);
+        std::cout << test.getTime().asSeconds() << "\n";
+
+        // Prints 0.500
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        test.stop(false);
+        std::cout << test.getTime().asSeconds() << "\n";
+
+        // Prints 0.750
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        std::cout << test.getTime().asSeconds() << "\n";
+
+        // Prints epoch
+        std::cout << fe::clock::getTimeSinceEpoch().asSeconds() << "\n";
+
+        // Prints current date
+        std::cout << fe::clock::getCurrentTime("%c %Z");
 
         std::cin.get();
         return 0;
