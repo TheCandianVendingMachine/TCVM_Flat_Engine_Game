07f6b339ea32c189e07518e48ba2951560c3c689
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-25 21:15:36 -0700

    Added fileUtility test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 // Flat Engine Game
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
+#include <fe/filesystem/fileUtilities.hpp>
+#include <iostream>
 
 int main()
     {
+        auto first = fe::lastTimeModified("test.txt");
+
+        std::ofstream out("test.txt");
+        out << "a";
+        out.close();
+        
+        std::cout << "Difference: " << fe::lastTimeModified("test.txt") - first;
+
+        std::cin.get();
         return 0;
     }
