2518502d8e754df3fc3179f36c8354210545a75a
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-26 18:56:10 -0700

    Shows how the pool allocater works

--- main.cpp
+++ main.cpp
@@ -2,49 +2,31 @@
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
 #include <iostream>
 #include <fe/subsystems/memory/memoryManager.hpp>
+#include <fe/subsystems/memory/poolAllocater.hpp>
 
 int main()
     {
         fe::memoryManager memManager;
-        memManager.startUp();
+        memManager.startUp(sizeof(int) * 5, sizeof(int));
 
-        int *unsafeAlloc = static_cast<int*>(FE_ALLOC_UNSAFE(sizeof(int)));
-        *unsafeAlloc = 15;
+        fe::poolAllocater<int> poolAlloc;
+        poolAlloc.startUp(2, memManager.getBuffer());
 
-        auto markerFirst = fe::memoryManager::get().getStackAllocater().getMarker();
-        int *stackAllocBeforeMarker = static_cast<int*>(FE_ALLOC_STACK(sizeof(int)));
-        *stackAllocBeforeMarker = 30;
+        int *i = poolAlloc.alloc();
+        *i = 5;
 
-        auto marker = fe::memoryManager::get().getStackAllocater().getMarker();
-        int *stackAllocAfterMarker = static_cast<int*>(FE_ALLOC_STACK(sizeof(int)));
-        *stackAllocAfterMarker = 45;
+        int *j = poolAlloc.alloc();
+        *j = 10;
 
-        std::cout << unsafeAlloc << " " << stackAllocBeforeMarker << " " << stackAllocAfterMarker << "\n";
-        std::cout << *unsafeAlloc << " " << *stackAllocBeforeMarker << " " << *stackAllocAfterMarker << "\n\n";
+        std::cout << i << " " << *i << " " << j << " " << *j << "\n";
 
-        // Shows how freeing the stack to a marker doesnt invalidate pointers
-        FE_FREE_STACK(marker);
-        std::cout << unsafeAlloc << " " << stackAllocBeforeMarker << " " << stackAllocAfterMarker << "\n";
-        std::cout << *unsafeAlloc << " " << *stackAllocBeforeMarker << " " << *stackAllocAfterMarker << "\n\n";
+        poolAlloc.clear();
+        std::cout << i << " " << *i << " " << j << " " << *j << "\n";
 
-        // Shows how once you free the stack you can overwrite the data
-        float *stackFree = static_cast<float*>(FE_ALLOC_STACK(sizeof(float)));
-        *stackFree = 60.f;
-
-        std::cout << unsafeAlloc << " " << stackAllocBeforeMarker << " " << stackAllocAfterMarker << " " << stackFree << "\n";
-        std::cout << *unsafeAlloc << " " << *stackAllocBeforeMarker << " " << *stackAllocAfterMarker << " " << *stackFree << "\n\n";
-
-        // Shows how freeing the stack doesnt invalidate pointers, it only allows them to be overwritten
-        FE_FREE_STACK(markerFirst);
-
-        std::cout << unsafeAlloc << " " << stackAllocBeforeMarker << " " << stackAllocAfterMarker << " " << stackFree << "\n";
-        std::cout << *unsafeAlloc << " " << *stackAllocBeforeMarker << " " << *stackAllocAfterMarker << " " << *stackFree << "\n\n";
+        j = poolAlloc.alloc();
+        std::cout << i << " " << *i << " " << j << " " << *j << "\n";
 
         memManager.shutDown();
-
-        // Shows how we can't show the data anymore since it has been freed
-        std::cout << unsafeAlloc << " " << stackAllocBeforeMarker << " " << stackAllocAfterMarker << " " << stackFree << "\n";
-
         std::cin.get();
         return 0;
     }
