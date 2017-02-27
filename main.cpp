7f6ca5491737b901d870a0fccf45696258aae0fc
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-26 23:01:02 -0700

    Added example for new file utility stuff

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
 // Flat Engine Game
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
 #include <iostream>
-#include <fe/debug/logger.hpp>
-#include <fe/subsystems/memory/memoryManager.hpp>
+#include <fe/subsystems/filesystem/fileUtilities.hpp>
 
 int main()
     {
-        fe::logger log;
-        log.startUp("output.txt");
+        std::cout << "All files in directory and subdirectories\n";
+        auto ret = fe::getAllFilesInDirectory("../");
+        for (auto &file : ret)
+            {
+                std::cout << file << "\n";
+            }
 
-        FE_LOG("Logging to console + output", 123);
-        FE_CONSOLE_LOG("Logging to only console", 456);
+        std::cout << "\nAll files in directory and subdirectories with extension .cpp\n";
+        ret = fe::getAllFilesInDirectory("../", ".cpp");
+        for (auto &file : ret)
+            {
+                std::cout << file << "\n";
+            }
 
-        fe::memoryManager mm;
-        mm.startUp(3, 0);
+        std::cout << "\nAll files in single directory\n";
+        ret = fe::getFilesInDirectory("../");
+        for (auto &file : ret)
+            {
+                std::cout << file << "\n";
+            }
 
-        mm.alloc(4);
+        std::cout << "\nAll files in directory with extension .sln\n";
+        ret = fe::getFilesInDirectory("../", ".sln");
+        for (auto &file : ret)
+            {
+                std::cout << file << "\n";
+            }
 
-        mm.shutDown();
-        log.shutDown();
         std::cin.get();
         return 0;
     }
