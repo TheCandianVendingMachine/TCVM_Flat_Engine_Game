04590bc8d3ddfee190ee65668ab2f8c269174818
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-27 23:18:17 -0700

    Shows example of how resource manager works

--- main.cpp
+++ main.cpp
@@ -1,38 +1,27 @@
 // Flat Engine Game
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
 #include <iostream>
-#include <fe/subsystems/filesystem/fileUtilities.hpp>
+#include <fe/subsystems/memory/memoryManager.hpp>
+#include <fe/subsystems/resourceManager/resourceObject.hpp>
+#include <fe/subsystems/resourceManager/resourceManager.hpp>
+#include <fe/debug/logger.hpp>
 
 int main()
     {
-        std::cout << "All files in directory and subdirectories\n";
-        auto ret = fe::getAllFilesInDirectory("../");
-        for (auto &file : ret)
-            {
-                std::cout << file << "\n";
-            }
+        fe::memoryManager memManager;
+        fe::logger logger;
+        fe::resourceManager resourceManager;
 
-        std::cout << "\nAll files in directory and subdirectories with extension .cpp\n";
-        ret = fe::getAllFilesInDirectory("../", ".cpp");
-        for (auto &file : ret)
-            {
-                std::cout << file << "\n";
-            }
+        ///////
+        memManager.startUp(sizeof(char) * 1024, sizeof(char) * 512);
+        logger.startUp("output.txt");
+        resourceManager.startUp(1);
 
-        std::cout << "\nAll files in single directory\n";
-        ret = fe::getFilesInDirectory("../");
-        for (auto &file : ret)
-            {
-                std::cout << file << "\n";
-            }
+        resourceManager.add("test", "test.png", fe::resourceObjectType::RESOURCE_TEXTURE);
 
-        std::cout << "\nAll files in directory with extension .sln\n";
-        ret = fe::getFilesInDirectory("../", ".sln");
-        for (auto &file : ret)
-            {
-                std::cout << file << "\n";
-            }
-
+        logger.shutDown();
+        memManager.shutDown();
+        ////////
         std::cin.get();
         return 0;
     }
