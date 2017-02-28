e89f8323c9d6744b5f2dffd333bf9d83734ae3a5
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-02-27 23:21:18 -0700

    Example now needs a memory manager and a logger from this point on

--- main.cpp
+++ main.cpp
@@ -2,26 +2,30 @@
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
 #include <iostream>
 #include <fe/subsystems/memory/memoryManager.hpp>
-#include <fe/subsystems/resourceManager/resourceObject.hpp>
-#include <fe/subsystems/resourceManager/resourceManager.hpp>
 #include <fe/debug/logger.hpp>
+
+fe::memoryManager memManager;
+fe::logger logger;
+
+void init()
+    {
+        memManager.startUp(sizeof(char) * 1024, sizeof(char) * 512);
+        logger.startUp("output.txt");
+    }
+
+void deinit()
+    {
+        logger.shutDown();
+        memManager.shutDown();
+    }
 
 int main()
     {
-        fe::memoryManager memManager;
-        fe::logger logger;
-        fe::resourceManager resourceManager;
+        init();
 
-        ///////
-        memManager.startUp(sizeof(char) * 1024, sizeof(char) * 512);
-        logger.startUp("output.txt");
-        resourceManager.startUp(1);
 
-        resourceManager.add("test", "test.png", fe::resourceObjectType::RESOURCE_TEXTURE);
 
-        logger.shutDown();
-        memManager.shutDown();
-        ////////
+        deinit();
         std::cin.get();
         return 0;
     }
