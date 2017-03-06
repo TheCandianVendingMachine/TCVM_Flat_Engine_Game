190111c34dcc959b35d6b19a6a2cee53c878f87f
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-03-06 15:48:38 -0700

    Showing how the new operator new works, as well as the engine

--- main.cpp
+++ main.cpp
@@ -1,42 +1,16 @@
 // Flat Engine Game
 // A game that will be made alongside the engine. Meant to reduce scope of the engine
 #include <iostream>
-#include <fe/subsystems/memory/memoryManager.hpp>
-#include <fe/debug/logger.hpp>
-
-#include <fe/subsystems/resourceManager/resourceManager.hpp>
-#include <fe/debug/profiler.hpp>
-
-fe::memoryManager memManager;
-fe::logger logger;
-
-void init()
-    {
-        logger.startUp("output.txt");
-        memManager.startUp(sizeof(char) * 1024, sizeof(char) * 512); 
-    }
-
-void deinit()
-    {
-        logger.shutDown();
-        memManager.shutDown();
-    }
+#include <fe/engine.hpp>
 
 int main()
     {
-        init();
+        fe::engine engine;
+        engine.startUp();
 
-        fe::resourceManager manager;
-        manager.startUp(2);
-        manager.add("123", "test.png", fe::resourceObjectType::RESOURCE_TEXTURE);
+        fe::memoryManager::get().printDebugInformation();
 
-        PROFILE("abc");
-        FE_ALLOC_DIRECT("poop", 464);
-        END_PROFILE;
-
-        memManager.printDebugInformation();
-
-        deinit();
+        engine.shutDown();
         std::cin.get();
         return 0;
     }
