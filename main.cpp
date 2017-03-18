5cdba6a4f1dd2563ac9560f80da6172dff819df7
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-03-18 00:52:51 -0600

    Updated engine to test out sprite batching

--- main.cpp
+++ main.cpp
@@ -14,18 +14,20 @@ class testEntity : public fe::baseEntity
             void moveLeft() { m_speed.x = -200.f; }
 
         public:
-            testEntity()
+            testEntity(fe::Vector2d position)
                 {
                     m_verticies[0].position = sf::Vector2f(0, 0);
-                    m_verticies[1].position = sf::Vector2f(100, 0);
-                    m_verticies[2].position = sf::Vector2f(100, 50);
-                    m_verticies[3].position = sf::Vector2f(0, 50);
+                    m_verticies[1].position = sf::Vector2f(20, 0);
+                    m_verticies[2].position = sf::Vector2f(20, 20);
+                    m_verticies[3].position = sf::Vector2f(0, 20);
 
                     m_verticies[0].color = sf::Color::Red;
                     m_verticies[2].color = sf::Color::Blue;
 
-                    fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::A, fe::function<void(testEntity*)>(this, &testEntity::moveLeft)));
-                    fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::D, fe::function<void(testEntity*)>(this, &testEntity::moveRight)));
+                    setPosition(position);
+
+                    //fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::A, fe::function<void(testEntity*)>(this, &testEntity::moveLeft)));
+                    //fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::D, fe::function<void(testEntity*)>(this, &testEntity::moveRight)));
                 }
 
             void update(float deltaTime)
@@ -41,20 +43,32 @@ class gameState : public fe::baseGameState
         public:
             void init()
                 {
-                    addEntity<testEntity>();
+                    for (int i = 0; i < 64; ++i)
+                        {
+                            for (int j = 0; j < 36; ++j) 
+                                {
+                                    addEntity<testEntity>(fe::Vector2d((i * 20) + 1, (j * 20) + 1));
+                                }
+                        }
                 }
     };
 
+fe::engine engine;
+
+void debug()
+    {
+        fe::memoryManager::get().printDebugInformation();
+        std::cout << "\nFPS: " << engine.getFPS() << "\n\n";
+    }
+
 int main()
     {
-        fe::engine engine;
         engine.startUp();
 
         gameState game;
         engine.queueState(&game);
 
-        fe::function<void(fe::memoryManager*)> callback(&fe::memoryManager::get(), &fe::memoryManager::printDebugInformation);
-        fe::inputManager::get().add(fe::input<sf::Keyboard::Key, fe::memoryManager*>(false, true, sf::Keyboard::Tilde, callback));
+        fe::inputManager::get().add(fe::input<sf::Keyboard::Key>(false, true, sf::Keyboard::Tilde, fe::function<void()>(debug)));
         engine.run();
 
         engine.shutDown();
