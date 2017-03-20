6c07149f71378141dbfbbd4e8477a9ecf4fd9979
Brandon Danyluk <brandondany@hotmail.co.uk>
2017-03-20 11:35:44 -0600

    Started work on Pong clone

---
+++ entities/paddle.hpp
@@ -0,0 +1,51 @@
+// paddle.hpp
+// a paddle entity
+#pragma once
+#include <fe/entity/baseEntity.hpp>
+#include <fe/subsystems/input/inputManager.hpp>
+#include <fe/collision/colliders.hpp>
+#include <fe/engine.hpp>
+
+class paddle : public fe::baseEntity
+    {
+        private:
+            fe::AABB m_bounds;
+            fe::Vector2d m_speed;
+
+            void moveUp() { m_speed.y = -200.f; }
+            void moveDown() { m_speed.y = 200.f; }
+
+        public:
+            paddle(fe::Vector2d position) : m_bounds(getPosition(), {20, 150})
+                {
+                    m_verticies[0].position = sf::Vector2f(0, 0);
+                    m_verticies[1].position = sf::Vector2f(20, 0);
+                    m_verticies[2].position = sf::Vector2f(20, 150);
+                    m_verticies[3].position = sf::Vector2f(0, 150);
+
+                    setColour(sf::Color::Blue);
+
+                    setPosition(position);
+
+                    fe::inputManager::get().add<paddle>(fe::input<sf::Keyboard::Key, paddle*>(true, true, sf::Keyboard::Up, fe::function<void(paddle*)>(this, &paddle::moveUp)));
+                    fe::inputManager::get().add<paddle>(fe::input<sf::Keyboard::Key, paddle*>(true, true, sf::Keyboard::Down, fe::function<void(paddle*)>(this, &paddle::moveDown)));
+                }
+
+            void update(float deltaTime)
+                {
+                    move(m_speed * deltaTime);
+                    m_speed = fe::Vector2d();
+                    
+                    if (getPosition().y > fe::engine::getWindowSize().y + 150.f)
+                        {
+                            setPosition({ getPosition().x, fe::engine::getWindowSize().y + 150.f });
+                        }
+
+                    if (getPosition().y < 0.f)
+                        {
+                            setPosition({ getPosition().x, 0.f });
+                        }
+                }
+
+            const fe::collider &getCollider() { return m_bounds; }
+    };
--- main.cpp
+++ main.cpp
@@ -3,43 +3,9 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <fe/engine.hpp>
-#include <fe/entity/baseEntity.hpp>
-#include <fe/collision/colliders.hpp>
-#include <fe/collision/collisionManager.hpp>
 #include <fe/subsystems/gameState/gameState.hpp>
 
-class testEntity : public fe::baseEntity
-    {
-        private:
-            fe::AABB m_bounds;
-            fe::Vector2d m_speed;
-
-            void moveRight() { m_speed.x = 200.f; }
-            void moveLeft() { m_speed.x = -200.f; }
-
-        public:
-            testEntity(fe::Vector2d position) : m_bounds(getPosition(), {20, 20})
-                {
-                    m_verticies[0].position = sf::Vector2f(0, 0);
-                    m_verticies[1].position = sf::Vector2f(20, 0);
-                    m_verticies[2].position = sf::Vector2f(20, 20);
-                    m_verticies[3].position = sf::Vector2f(0, 20);
-
-                    m_verticies[0].color = sf::Color::Red;
-                    m_verticies[2].color = sf::Color::Blue;
-
-                    setPosition(position);
-
-                    fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::A, fe::function<void(testEntity*)>(this, &testEntity::moveLeft)));
-                    fe::inputManager::get().add<testEntity>(fe::input<sf::Keyboard::Key, testEntity*>(true, true, sf::Keyboard::D, fe::function<void(testEntity*)>(this, &testEntity::moveRight)));
-                }
-
-            void update(float deltaTime)
-                {
-                    move(m_speed * deltaTime);
-                    m_speed = fe::Vector2d();
-                }
-    };
+#include "entities/paddle.hpp"
 
 class gameState : public fe::baseGameState
     {
@@ -47,7 +13,7 @@ class gameState : public fe::baseGameState
         public:
             void init()
                 {
-                    addEntity<testEntity>(fe::Vector2d(20, 20));
+                    addEntity<paddle>(fe::Vector2d(40, 0));
                 }
     };
 
