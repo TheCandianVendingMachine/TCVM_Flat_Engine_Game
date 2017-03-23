26ae7afae72b6477116063b9bc22542f9c8f42d2
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-03-23 17:22:58 -0600

    Colisions updated and work

--- entities/ball.cpp
+++ entities/ball.cpp
@@ -1,6 +1,6 @@
 #include "ball.hpp"
 
-void ball::onCollision(const fe::collider *collision)
+void ball::onCollision(const fe::collider &collision)
     {
         m_speed.x = -m_speed.x;
     }
@@ -17,8 +17,7 @@ ball::ball(fe::Vector2d position) : m_bounds(getPosition(), { 20, 20 })
         setColour(sf::Color::Blue);
 
         setPosition(position);
-
-        m_bounds.m_callback = fe::function<void, ball, const fe::collider*>(fe::fPtr<true, ball, void, const fe::collider*>(this, &ball::onCollision));
+        m_bounds.m_callback = fe::function<void, ball, const fe::collider&>(fe::fPtr<true, ball, void, const fe::collider&>(this, &ball::onCollision));
     }
 
 void ball::update(float deltaTime)
