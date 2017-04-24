4921efadec9388eb727b230f59b76e7424eb3ab4
Brandon Danyluk <brandondany@hotmail.co.uk>
2017-04-24 11:32:15 -0600

    Started work on the main menu

--- gameStates/mainMenu.cpp
+++ gameStates/mainMenu.cpp
@@ -11,11 +11,20 @@ void mainMenu::init()
         m_gui.setSize(fe::engine::get().getWindowSize());
 
         sf::Font *font = m_font.load("Roboto-Regular.ttf", "RobotFont");
-        auto but = m_gui.addElement(new fe::gui::button(fe::Vector2d(64, 32), []() { fe::engine::get().getStateMachine().queuePop(); fe::engine::get().getStateMachine().queuePush<gameState>(); }));
-        auto label = m_gui.addElement(new fe::gui::label(*font, "Test1"));
+        auto title = m_gui.addElement(new fe::gui::label(*font, "Flat Engine Pong"));
+        static_cast<fe::gui::label*>(m_gui.getElement(title))->setCharacterSize(72);
+        m_gui.setElementPosition(title, { 0.5f, 0.15f });
 
-        m_gui.getElement(label)->setParent(m_gui.getElement(but));
-        m_gui.setElementPosition(but, fe::Vector2d(0.5f, 0.5f));
+        auto startButtonHandle = m_gui.addElement(new fe::gui::button(fe::Vector2d(200, 35), []() { fe::engine::get().getStateMachine().queuePop(); fe::engine::get().getStateMachine().queuePush<gameState>(); }));
+        auto startLabelHandle = m_gui.addElement(new fe::gui::label(*font, "Start Game"));
+
+        auto startButton = m_gui.getElement(startButtonHandle);
+        auto startLabel = m_gui.getElement(startLabelHandle);
+
+        startLabel->setParent(startButton);
+        startLabel->setPosition(startButton->getSize() / 2.f);
+
+        m_gui.setElementPosition(startButtonHandle, fe::Vector2d(0.5f, 0.5f));
 
         addPanel(&m_gui);
     }
