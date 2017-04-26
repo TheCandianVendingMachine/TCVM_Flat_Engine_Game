290a25da86fb59fbc0bad333530ac7a7d7247fda
Brandon Danyluk <brandondany@hotmail.co.uk>
2017-04-26 13:17:05 -0600

    Started work on GUI for pause menu

--- gameStates/pauseMenu.cpp
+++ gameStates/pauseMenu.cpp
@@ -8,4 +8,20 @@
 
 void pauseMenu::init()
     {
+        auto pauseText = m_gui.addElement(new fe::gui::label(*m_font.get("robot"), "Paused"));
+        auto resumeText = m_gui.addElement(new fe::gui::label(*m_font.get("robot"), "Resume"));
+        auto exitMenuText = m_gui.addElement(new fe::gui::label(*m_font.get("robot"), "Exit to Menu"));
+
+        auto resumeButton = m_gui.addElement(new fe::gui::button(fe::Vector2d(150, 30), []() {fe::engine::get().getStateMachine().queuePop(); }));
+        auto exitToMenu = m_gui.addElement(new fe::gui::button(fe::Vector2d(150, 30), []() {fe::engine::get().getStateMachine().clear(); fe::engine::get().getStateMachine().queuePush<mainMenu>(); }));
+
+        m_gui.getElement(resumeText)->setParent(m_gui.getElement(resumeButton));
+        m_gui.getElement(exitMenuText)->setParent(m_gui.getElement(exitToMenu));
+
+        m_gui.setElementPosition(resumeButton, fe::Vector2d(0.5, 0.5));
+        m_gui.setElementPosition(exitMenuText, fe::Vector2d(0.5, 0.55));
+
+        m_gui.setElementPosition(pauseText, fe::Vector2d(0.5, 0.4));
+
+        static_cast<fe::gui::label*>(m_gui.getElement(pauseText))->setCharacterSize(48);
     }
